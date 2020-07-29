e533441b1b926d71f35f933a318cef0a46588245
Marisa Doig <mcr.doig@gmail.com>
2020-07-29 21:59:34 +0000

    Added function to read user input and terminate on ctrl-C

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+setupInput();

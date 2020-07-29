740e0af92b07c43c38d43b7a467b3f773f257462
Marisa Doig <mcr.doig@gmail.com>
2020-07-29 22:41:59 +0000

    Moved IP & port values to separate constants file

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
   conn.on('connect', (connect) => {
     console.log('Successfully connected to game server');

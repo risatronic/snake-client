5eba5d853725c60ed24aa7afc567cd0bdb06b4eb
Marisa Doig <mcr.doig@gmail.com>
2020-07-29 22:24:35 +0000

    changed handleUserInput function to be less wordy

--- input.js
+++ input.js
@@ -1,10 +1,29 @@
+let connection;
+
 const handleUserInput = (input) => {
-  if (input === '\u0003') {
+  switch (input) {
+  case '\u0003':
     process.exit();
+    break;
+  case 'w':
+    connection.write("Move: up");
+    break;
+  case 'a':
+    connection.write("Move: left");
+    break;
+  case 's':
+    connection.write("Move: down");
+    break;
+  case 'd':
+    connection.write("Move: right");
+    break;
+  default:
+    break;
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
 
   stdin.setRawMode(true);
